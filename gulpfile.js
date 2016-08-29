df4d67acc11d109dac4984653fed9a8464e64c7f
rokfortuna <rokfortunaslo@gmail.com>
2016-08-29 10:47:07 +0200

    Port added from Heroku.

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,7 @@ gulp.task('watch', function() {
 
 gulp.task('serve-prod', serve({
   root: ['dist'],
-  port: 80,
+  port: $PORT,
   https: true,
   middleware: function(req, res) {
     // custom optional middleware
