42b7177d1b8e3aa42101b2f0af36e8cb9b0264ae
rokfortuna <rokfortunaslo@gmail.com>
2016-09-26 16:09:54 +0200

    Removed triggers etc.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3015,17 +3015,14 @@ $(function() {
 	// build scenes
 	new ScrollMagic.Scene({triggerElement: "#menu-toggle-indicator", })
 					.setClassToggle("#top-nav", "white") // add class toggle
-          .addIndicators()
 					.addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#menu-toggle-indicator", offset: $(window).innerHeight()/2 - 50})
           .setClassToggle(".burger-line", 'green')
-          .addIndicators()
   				.addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#nav-home-trigger", duration: $(".landing-home").height() + 32})
 					.setClassToggle("#nav-home", "active") // add class toggle
-          .addIndicators()
 					.addTo(controller);
 
 
@@ -3037,26 +3034,21 @@ $(function() {
 
   new ScrollMagic.Scene({triggerElement: "#nav-about-trigger", duration: $(".landing-about").height() + viewVisible * 6 + viewTransition * 5 + baseOffset - 60 })
 					.setClassToggle("#nav-about", "active") // add class toggle
-          .addIndicators()
 					.addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#nav-blog-trigger", duration: $(".landing-blog").height() + $(".blog").height() + 98})
 					.setClassToggle("#nav-blog", "active") // add class toggle
-          .addIndicators()
 					.addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#nav-media-trigger", duration: $(".landing-press").height()})
   				.setClassToggle("#nav-media", "active") // add class toggle
-          .addIndicators()
   				.addTo(controller);
 
   new ScrollMagic.Scene({triggerElement: "#nav-support-trigger", duration: $(".landing-support").height()})
   				.setClassToggle("#nav-support", "active") // add class toggle
-          .addIndicators()
   				.addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#nav-launch-trigger", duration: $(".landing-launch").height()})
   				.setClassToggle("#nav-launch", "active") // add class toggle
-          .addIndicators()
   				.addTo(controller);
 
   $(document).on("click", ".nav-item", function (e) {
