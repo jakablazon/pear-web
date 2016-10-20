02d37134269ce4b0e8ce8fee9e1fefee48d27d3f
Jure Cindro <jure@elpy.io>
2016-10-20 14:59:50 +0200

    Added header

--- wp-content/themes/pear/templates/header.php
+++ wp-content/themes/pear/templates/header.php
@@ -1,12 +1,40 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header id="top-nav" class="top-nav">
+	<a href="<?= esc_url( home_url( '/' ) ); ?>"><span class="pear-logo"></span></a>
+	<nav class="nav-container">
+		<div id="nav-home" class="nav-item clickable">
+			Home
+		</div>
+		<div id="nav-about" class="nav-item clickable">
+			About
+		</div>
+		<div id="nav-blog" class="nav-item clickable">
+			Blog
+		</div>
+		<div id="nav-support" class="nav-item clickable">
+			Q &amp; A
+		</div>
+	</nav>
+
+	<?php // TODO: a bo to delalo? ?>
+	<div class="burger-menu-overlay">
+		<div></div>
+		<div id="nav-home" class="nav-item clickable">
+			Home
+		</div>
+		<div id="nav-about" class="nav-item clickable">
+			About
+		</div>
+		<div id="nav-blog" class="nav-item clickable">
+			Blog
+		</div>
+		<div id="nav-support" class="nav-item clickable">
+			Q &amp; A
+		</div>
+		<div></div>
+	</div>
+	<div class="burger-menu">
+		<div class="burger-line burger-line-top"></div>
+		<div class="burger-line burger-line-middle"></div>
+		<div class="burger-line burger-line-bottom"></div>
+	</div>
 </header>
