89eefed6656ef241a877768e32aa75357270507d
Jure Cindro <jure@elpy.io>
2016-10-21 12:05:15 +0200

    Dryied up content

--- wp-content/themes/pear/index.php
+++ wp-content/themes/pear/index.php
@@ -153,9 +153,7 @@
 				</div>
 				<div class="slick-carousel-container">
 					<div class="slick-carousel">
-						<?php while ( have_posts() ) : the_post(); ?>
-							<?php get_template_part( 'templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format() ); ?>
-						<?php endwhile; ?>
+						<?php get_template_part( 'templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format() ); ?>
 					</div>
 				</div>
 			</div>
--- wp-content/themes/pear/templates/content.php
+++ wp-content/themes/pear/templates/content.php
@@ -1,4 +1,6 @@
-<div <?php post_class( 'text-center post' ); ?>>
-	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
-	<p class="text-size-smd text-capitalize pl32 pr32 pb24"><?php the_title(); ?></p>
-</div>
+<?php while ( have_posts() ) : the_post(); ?>
+	<div <?php post_class( 'text-center post' ); ?>>
+		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
+		<p class="text-size-smd text-capitalize pl32 pr32 pb24"><?php the_title(); ?></p>
+	</div>
+<?php endwhile; ?>
