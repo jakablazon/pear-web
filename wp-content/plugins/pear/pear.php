11d8aa63f7e03618ca657079b657a57a327bd958
Jure Cindro <jure@elpy.io>
2016-10-24 20:23:19 +0200

    Add: admin page for subscriptions

--- wp-content/plugins/pear/pear.php
+++ wp-content/plugins/pear/pear.php
@@ -7,18 +7,90 @@ Author: PovioLabs
 Version: 1.0.0
 */
 
-$config = array(
-	'psf_db_version'   => '1.0',
-	'psf_db_tablename' => 'psf',
-);
-
 class PearSubscriptionForm {
-	var $psf_db_version;
-	var $psf_db_tablename;
 
-	function __constructor( $config ) {
-		$this->psf_db_version   = $config['psf_db_version'];
-		$this->psf_db_tablename = $config['psf_db_tablename'];
+	// class instance
+	static $instance;
+
+	// subscriber WP_List_Table object
+	public $subscription_obj;
+
+	// class constructor
+	public function __construct() {
+		add_filter( 'set-screen-option', [ __CLASS__, 'set_screen' ], 10, 3 );
+		add_action( 'admin_menu', [ $this, 'plugin_menu' ] );
+	}
+
+	public static function set_screen( $status, $option, $value ) {
+		return $value;
+	}
+
+	public function plugin_menu() {
+
+		$hook = add_menu_page(
+			__( 'Subscriptions', 'pear' ),
+			__( 'Subscriptions', 'pear' ),
+			'edit_posts',
+			'pear-subscriptions',
+			[ $this, 'plugin_settings_page' ],
+			'dashicons-editor-ul',
+			6
+		);
+
+		add_action( "load-$hook", [ $this, 'screen_option' ] );
+
+	}
+
+	/**
+	 * Screen options
+	 */
+	public function screen_option() {
+
+		$option = 'per_page';
+		$args   = [
+			'label'   => 'Subscriptions',
+			'default' => 10,
+			'option'  => 'subscribers_per_page'
+		];
+
+		add_screen_option( $option, $args );
+
+		$this->subscription_obj = new Subscriptions_List();
+	}
+
+	/**
+	 * Plugin settings page
+	 */
+	public function plugin_settings_page() {
+		?>
+		<div class="wrap">
+			<h2>Subscriptions</h2>
+
+			<div id="poststuff">
+				<div id="post-body" class="metabox-holder columns-2">
+					<div id="post-body-content">
+						<div class="meta-box-sortables ui-sortable">
+							<form method="post">
+								<?php
+								$this->subscription_obj->prepare_items();
+								$this->subscription_obj->display(); ?>
+							</form>
+						</div>
+					</div>
+				</div>
+				<br class="clear">
+			</div>
+		</div>
+		<?php
+	}
+
+	/** Singleton instance */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
 	}
 
 	static function psf_install() {
@@ -64,7 +136,7 @@ class PearSubscriptionForm {
 			</div>
 			<div id="psf-submit" class="button green shadow text-uppercase button-join mt40">
 				<span class="join-text">Join now</span>
-				<img class="loader inactive" src="<?php echo get_template_directory_uri(); ?>/dist/images/loader.svg" />
+				<img class="loader inactive" src="<?php echo get_template_directory_uri(); ?>/dist/images/loader.svg"/>
 			</div>
 			<p class="mt24 we-promise">We promise to keep our emails to a minimum.</p>
 			<p class="mt0 mb32">Join our community to be among our beta testers or know when Pear will be available
@@ -76,7 +148,247 @@ class PearSubscriptionForm {
 
 }
 
-new PearSubscriptionForm( $config );
+add_action( 'plugins_loaded', function () {
+	PearSubscriptionForm::get_instance();
+} );
 
 register_activation_hook( __FILE__, array( 'PearSubscriptionForm', 'psf_install' ) );
 add_shortcode( 'PearSubForm', array( 'PearSubscriptionForm', 'form' ) );
+
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+class Subscriptions_List extends WP_List_Table {
+
+	/** Class constructor */
+	public function __construct() {
+
+		parent::__construct( [
+			'singular' => __( 'Subscription', 'pear' ), //singular name of the listed records
+			'plural'   => __( 'Subscriptions', 'pear' ), //plural name of the listed records
+			'ajax'     => false //should this table support ajax?
+
+		] );
+
+	}
+
+	/**
+	 * Retrieve subscriber’s data from the database
+	 *
+	 * @param int $per_page
+	 * @param int $page_number
+	 *
+	 * @return mixed
+	 */
+	public static function get_subscriptions( $per_page = 10, $page_number = 1 ) {
+
+		global $wpdb;
+
+		$sql = "SELECT * FROM {$wpdb->prefix}psf";
+
+		if ( ! empty( $_REQUEST['orderby'] ) ) {
+			$sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
+			$sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
+		}
+
+		$sql .= " LIMIT $per_page";
+
+		$sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
+
+
+		$result = $wpdb->get_results( $sql, 'ARRAY_A' );
+
+		return $result;
+	}
+
+	/**
+	 * Delete a subscriber record.
+	 *
+	 * @param int $id subscriber ID
+	 */
+	public static function delete_subscription( $id ) {
+		global $wpdb;
+
+		$wpdb->delete(
+			"{$wpdb->prefix}psf",
+			[ 'ID' => $id ],
+			[ '%d' ]
+		);
+	}
+
+	/**
+	 * Returns the count of records in the database.
+	 *
+	 * @return null|string
+	 */
+	public static function record_count() {
+		global $wpdb;
+
+		$sql = "SELECT COUNT(*) FROM {$wpdb->prefix}psf";
+
+		return $wpdb->get_var( $sql );
+	}
+
+	/** Text displayed when no subscriber data is available */
+	public function no_items() {
+		_e( 'No subscriptions avaliable.', 'pear' );
+	}
+
+	/**
+	 * Method for name column
+	 *
+	 * @param array $item an array of DB data
+	 *
+	 * @return string
+	 */
+	function column_name( $item ) {
+
+		// create a nonce
+		$delete_nonce = wp_create_nonce( 'psf_delete_subscriber' );
+
+		$title = '<strong>' . $item['Name'] . '</strong>';
+
+		$actions = [
+			'delete' => sprintf( '<a href="?page=%s&action=%s&subscriber=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['ID'] ), $delete_nonce )
+		];
+
+		return $title . $this->row_actions( $actions );
+	}
+
+	/**
+	 * Render a column when no column specific method exists.
+	 *
+	 * @param array $item
+	 * @param string $column_name
+	 *
+	 * @return mixed
+	 */
+	public function column_default( $item, $column_name ) {
+		switch ( $column_name ) {
+			case 'Email':
+			case 'Name':
+			case 'Age':
+				return $item[ $column_name ];
+			default:
+				return print_r( $item, true ); //Show the whole array for troubleshooting purposes
+		}
+	}
+
+	/**
+	 * Render the bulk edit checkbox
+	 *
+	 * @param array $item
+	 *
+	 * @return string
+	 */
+	function column_cb( $item ) {
+		return sprintf(
+			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['ID']
+		);
+	}
+
+	/**
+	 *  Associative array of columns
+	 *
+	 * @return array
+	 */
+	function get_columns() {
+		$columns = [
+			'cb'    => '<input type="checkbox" />',
+			'name'  => __( 'Name', 'pear' ),
+			'Email' => __( 'Email', 'pear' ),
+			'Age'   => __( 'Age', 'pear' )
+		];
+
+		return $columns;
+	}
+
+	/**
+	 * Columns to make sortable.
+	 *
+	 * @return array
+	 */
+	public function get_sortable_columns() {
+		$sortable_columns = array(
+			'Name'  => array( 'Name', true ),
+			'Email' => array( 'Email', false )
+		);
+
+		return $sortable_columns;
+	}
+
+	/**
+	 * Returns an associative array containing the bulk action
+	 *
+	 * @return array
+	 */
+	public function get_bulk_actions() {
+		$actions = [
+			'bulk-delete' => 'Delete'
+		];
+
+		return $actions;
+	}
+
+	/**
+	 * Handles data query and filter, sorting, and pagination.
+	 */
+	public function prepare_items() {
+
+		$this->_column_headers = $this->get_column_info();
+
+		/** Process bulk action */
+		$this->process_bulk_action();
+
+		$per_page     = $this->get_items_per_page( 'subscriptions_per_page', 10 );
+		$current_page = $this->get_pagenum();
+		$total_items  = self::record_count();
+
+		$this->set_pagination_args( [
+			'total_items' => $total_items, //WE have to calculate the total number of items
+			'per_page'    => $per_page //WE have to determine how many items to show on a page
+		] );
+
+
+		$this->items = self::get_subscriptions( $per_page, $current_page );
+	}
+
+	public function process_bulk_action() {
+
+		//Detect when a bulk action is being triggered...
+		if ( 'delete' === $this->current_action() ) {
+
+			// In our file that handles the request, verify the nonce.
+			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
+
+			if ( ! wp_verify_nonce( $nonce, 'psf_delete_subscriber' ) ) {
+				die( 'Not authorized!' );
+			} else {
+				self::delete_subscription( absint( $_GET['subscriber'] ) );
+
+				wp_redirect( esc_url( add_query_arg() ) );
+				exit;
+			}
+
+		}
+
+		// If the delete bulk action is triggered
+		if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-delete' )
+		     || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
+		) {
+
+			$delete_ids = esc_sql( $_POST['bulk-delete'] );
+
+			// loop over the array of record IDs and delete them
+			foreach ( $delete_ids as $id ) {
+				self::delete_subscriber( $id );
+
+			}
+
+			wp_redirect( esc_url( add_query_arg() ) );
+			exit;
+		}
+	}
+}
