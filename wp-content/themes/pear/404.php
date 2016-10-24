7fce73c75deb65353a85161a70fb9e66a5a78f0f
Jure Cindro <jure@elpy.io>
2016-10-24 18:21:42 +0200

    Add: 404 page

--- wp-content/themes/pear/404.php
+++ wp-content/themes/pear/404.php
@@ -1,7 +1,20 @@
 <?php get_template_part( 'templates/page', 'header' ); ?>
 
-<div class="alert alert-warning">
-	<?php _e( 'Sorry, but the page you were trying to view does not exist.', 'pear' ); ?>
+<div class="error-page pl16 pr16 ">
+	<div class="content">
+		<div class="row flex--justify-center">
+			<div class="col-xs-12 col-sm-8">
+				<div class="row mt96 pt40">
+					<div class="col-xs-12 col-sm-6">
+						<p class="text-size-xxxlg mt8 mb16">Oops!</p>
+						<p class="text-size-sm">We can't seem to find the page you're looking for.</p>
+						<p class="text-size-sm">Error code: 404</p>
+					</div>
+					<div class="col-xs-12 col-sm-6 mt40">
+						<div class="pear-logo"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-
-<?php get_search_form(); ?>
