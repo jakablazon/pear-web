51fffbcecb9a4a33245649bc91af11e48474631f
Jure Cindro <jure@elpy.io>
2016-11-07 09:30:04 +0100

    Fix: footer press kit link

--- wp-content/themes/pear/templates/footer.php
+++ wp-content/themes/pear/templates/footer.php
@@ -1,15 +1,14 @@
 <footer class="flex--justify-center pt32 pb32 text-center">
-	<a class="text-decoration-none text-color-grey-light text-thin text-size-xxs"
-	   href="https://www.dropbox.com/sh/n8ocqhghj0r0gmd/AAComVxf6TKP9sjD0XX2mTeAa?dl=0">Press kit</a>
-	<span class="bullet ml16 mr16"></span>
-	<a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="/terms">Terms and
-		conditions</a>
-	<span class="bullet ml16 mr16"></span>
-	<a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="/privacy">Privacy</a>
-	<span class="bullet ml16 mr16"></span>
-	<a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="mailto:support@pear.me"
-	   target="_blank">Contact</a>
-	<span class="bullet ml16 mr16"></span>
-	<a class="text-decoration-none text-color-grey-light text-thin text-size-xxs"
-	   href="http://www.algorithmicmatching.com/" target="_blank" rel="nofollow">Algorithmic Matching</a>
+  <a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="mailto:press@pear.me"
+     target="_blank">Press kit</a>
+  <span class="bullet ml16 mr16"></span>
+  <a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="/terms">Terms and conditions</a>
+  <span class="bullet ml16 mr16"></span>
+  <a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="/privacy">Privacy</a>
+  <span class="bullet ml16 mr16"></span>
+  <a class="text-decoration-none text-color-grey-light text-thin text-size-xxs" href="mailto:support@pear.me"
+     target="_blank">Contact</a>
+  <span class="bullet ml16 mr16"></span>
+  <a class="text-decoration-none text-color-grey-light text-thin text-size-xxs"
+     href="http://www.algorithmicmatching.com/" target="_blank" rel="nofollow">Algorithmic Matching</a>
 </footer>
