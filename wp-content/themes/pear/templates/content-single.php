63e3f4736d19bc73230222379e36371e1f85cb4f
Jure Cindro <jure@elpy.io>
2016-11-29 11:56:43 +0100

    Include super socializer in single content

--- wp-content/themes/pear/templates/content-single.php
+++ wp-content/themes/pear/templates/content-single.php
@@ -1,33 +1,37 @@
 <?php while ( have_posts() ) : the_post(); ?>
-	<article <?php post_class( 'row flex--justify-center' ); ?>>
-		<?php if ( have_rows( 'content' ) ): ?>
+  <article <?php post_class( 'row flex--justify-center' ); ?>>
+    <?php if ( have_rows( 'content' ) ): ?>
 
-			<?php while ( have_rows( 'content' ) ) : the_row(); ?>
+      <?php while ( have_rows( 'content' ) ) : the_row(); ?>
 
-				<?php if ( get_row_layout() == 'strong_text' ) : ?>
+        <?php if ( get_row_layout() == 'strong_text' ) : ?>
 
-					<div class="col-xs-12 col-sm-8">
-						<p class="mb64 text-size-smd"><?php the_sub_field( 'strong' ); ?></p>
-					</div>
+          <div class="col-xs-12 col-sm-8">
+            <p class="mb64 text-size-smd"><?php the_sub_field( 'strong' ); ?></p>
+          </div>
 
-				<?php elseif ( get_row_layout() == 'paragraph' ) : ?>
+        <?php elseif ( get_row_layout() == 'paragraph' ) : ?>
 
-					<div class="col-xs-12 col-sm-8">
-						<p class="mt0 mb40 text-thin text-line-height-md"><?php the_sub_field( 'p' ); ?></p>
-					</div>
+          <div class="col-xs-12 col-sm-8">
+            <p class="mt0 mb40 text-thin text-line-height-md"><?php the_sub_field( 'p' ); ?></p>
+          </div>
 
-				<?php elseif ( get_row_layout() == 'image' ) : ?>
+        <?php elseif ( get_row_layout() == 'image' ) : ?>
 
-					<div class="col-xs-12 col-sm-10">
-						<img class="text-center mt0 mb40" style="width:100%" src="<?php the_sub_field( 'img' ); ?>"/>
-					</div>
+          <div class="col-xs-12 col-sm-10">
+            <img class="text-center mt0 mb40" style="width:100%" src="<?php the_sub_field( 'img' ); ?>"/>
+          </div>
 
-				<?php endif;
+        <?php endif;
 
-			endwhile;
+      endwhile;
 
-		endif; ?>
-	</article>
+    endif; ?>
+
+    <div class="col-xs-12 col-sm-8">
+      <?php echo do_shortcode( '[TheChamp-Sharing]' ); ?>
+    </div>
+  </article>
 <?php endwhile; ?>
 
 
